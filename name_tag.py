4dc73d48e790c7d834cccfae0fdc747d799e4f7f
gaurnitai1 <suryawanshisandeep9@gmail.com>
2022-08-03 10:52:30 +0530

    Adding this program to githun

---
+++ name_tag.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def name_tag(first,last):
+    last_tag=last[:1]
+    print(first,last_tag+".")
+name_tag("Krishna","Yadav")
+name_tag("Jane", "Smith")
+name_tag("Jean-Luc", "Grand-Pierre")
